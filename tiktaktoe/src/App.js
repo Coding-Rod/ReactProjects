29bdfa0390fab49e113c2ae3ad863e957a40df21
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-08-20 17:39:08 -0400

    :bookmark: [RELEASE] 1.0.0

--- tiktaktoe/src/App.js
+++ tiktaktoe/src/App.js
@@ -2,24 +2,46 @@ import { useState } from 'react'
 import { Board } from './components/Board'
 
 export default function Game() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)])
   const [xIsNext, setXIsNext] = useState(true);
+  const squares = history[history.length - 1]
   
   function handlePlay(winner, nextSquares){
     if (winner || squares.every(square => square)){
-      // Reset the game
-      setSquares(Array(9).fill(null))
+      setHistory([Array(9).fill(null)])
       setXIsNext(true)
       return
     }
     setXIsNext(!xIsNext)
-    setSquares(nextSquares)
+    setHistory([...history, nextSquares])
   }
+
+  function jumpTo(step) {
+    setHistory(history.slice(0, step + 1))
+    setXIsNext(step % 2 === 0)
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : 'Go to game start'
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    }
+  )
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={squares} onPlay={handlePlay} />
+        <Board 
+          xIsNext={xIsNext} 
+          squares={squares} 
+          onPlay={handlePlay} 
+        />
+      </div>
+      <div className="game-info">
+        <ol>{ moves }</ol>
       </div>
     </div>
   )
--- tiktaktoe/src/components/Square.js
+++ tiktaktoe/src/components/Square.js
@@ -1,4 +1,3 @@
-
 export function Square({value, onClick}){
 
     return (
