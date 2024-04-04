bf247e79121fd339e236805f86f536ea1d61fbbd
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2024-04-03 23:16:24 -0400

    :tada: [START] New project

---
+++ introduction/src/App.js
@@ -0,0 +1,29 @@
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <TodoCounter 
+        completed={16}
+        total={25}
+      />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+      
+      <CreateTodoButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ introduction/src/TodoCounter.js
@@ -0,0 +1,10 @@
+
+function TodoCounter({ completed, total }) {
+  return (
+    <h1>
+      Has completado { completed } de { total } TODOs
+    </h1>
+  );
+}
+
+export { TodoCounter };
