38299c16e06eb842f88ee92e177c85831476815a
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2024-04-24 00:01:10 -0400

    :adhesive_bandage: [FIX] Tailwind separated class

--- first_twcss_proj/src/components/Button.jsx
+++ first_twcss_proj/src/components/Button.jsx
@@ -1,10 +1,8 @@
 const Button = ({ text, onClick }) => {
     
-    const buttonClass = "bg-orange-500 hover:bg-orange-800 hover:text-white font-bold py-2 px-4 rounded text-black";
-
     return (
         <button 
-            className={buttonClass}
+            className="bg-orange-500 hover:bg-orange-800 hover:text-white font-bold py-2 px-4 rounded text-black"
             onClick={onClick}
         >
             {text}
--- first_twcss_proj/src/components/Card.jsx
+++ first_twcss_proj/src/components/Card.jsx
@@ -2,7 +2,7 @@ const Card = ({ count }) => {
     return (
         <div className="shadow-md rounded-lg p-4 text-black bg-amber-700">
             <h2 className="text-xl font-bold mb-2">The count is</h2>
-            <p className="text-gray text-3xl">{count}</p>
+            <p className="text-gray text-xl">{count}</p>
         </div>
     );
 };
