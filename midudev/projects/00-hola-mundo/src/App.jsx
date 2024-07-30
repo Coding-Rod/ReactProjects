51007029136300807599350703c473c1cb47efd4
Coding-Rod <sergiorodrigofernandeztesta@gmail.com>
2024-07-29 23:04:41 -0400

    ✨ [CREATE] Follow card on App.jsx

---
+++ midudev/projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+export function App() {
+    return (
+        <article className="tw-followCard">
+            <header className="tw-followCard-header">
+                <img
+                    className="tw-followCard-avatar"
+                    alt="La imagen por defecto"
+                    src="https://unavatar.io/midudev"
+                />
+                <div className="tw-followCard-info">
+                    <strong>Miguel Angel Durán</strong>
+                    <span className="tw-followCard-infoUserName">@midudev</span>
+                </div>
+            </header>
+            <aside>
+                <button className="tw-followCard-button">
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    );
+}
