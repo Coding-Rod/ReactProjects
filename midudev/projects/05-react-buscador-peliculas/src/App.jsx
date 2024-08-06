3ff1363c1c6ceef59d216f56409cad76f16e506e
Coding-Rod <sergiorodrigofernandeztesta@gmail.com>
2024-08-05 21:48:51 -0400

    ✨ [ADD] useRef hook

--- midudev/projects/05-react-buscador-peliculas/src/App.jsx
+++ midudev/projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,16 +1,25 @@
 import "./App.css";
+import { useRef } from "react";
 import { Movies } from './components/Movies';
 import { useMovies } from './hooks/useMovies.js';
 
 function App() {
   const { movies } = useMovies()
+  const inputRef = useRef()
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const value = inputRef.current.value
+    console.log(value)
+
+  }
+ 
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
-        <form className="form">
-          <input type="text" placeholder="Avengers, Star Wars, The Matrix..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input ref={inputRef} type="text" placeholder="Avengers, Star Wars, The Matrix..." />
           <button type="submit">Buscar</button>
         </form>
       </header>
