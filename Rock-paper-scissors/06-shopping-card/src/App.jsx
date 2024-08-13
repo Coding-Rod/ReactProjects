110d9b671cbea2c53653e421bfa5e863247ff707
Coding-Rod <sergiorodrigofernandeztesta@gmail.com>
2024-08-13 15:18:10 -0400

    ✨ [ADD] filter

--- Rock-paper-scissors/06-shopping-card/src/App.jsx
+++ Rock-paper-scissors/06-shopping-card/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Products } from "./components/Products";
+import { products as initialProducts } from "./mocks/products.json";
+import { useMemo } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const filteredProducts = useMemo(() => {
+    return products.filter((product) => {
+      return (
+        // First filter
+        product.price >= filters.minPrice &&
+        // Second filter
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+    },[products,filters])
+
+  return <Products products={filteredProducts} />;
 }
 
-export default App
+export default App;
