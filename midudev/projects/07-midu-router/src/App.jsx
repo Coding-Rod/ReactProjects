885c59bb052a54e607b90d8fd4d72bcafe2f1e32
Coding-Rod <sergiorodrigofernandeztesta@gmail.com>
2024-08-22 15:52:05 -0400

    ✨ [ADD] MPA

--- midudev/projects/07-midu-router/src/App.jsx
+++ midudev/projects/07-midu-router/src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+function HomePage () {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
+      <a href="/about">Ir a Sobre nosotros</a>
+    </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About</h1>
+      <div><img src="https://pbs.twimg.com/profile_images/1531424650823188481/PgYdrPIN_400x400.jpg" alt="Foto de Rodrigo"/></div>
+      <p>Hola, estoy creando un clon de React-Router</p>
+      <a href="/">Ir a la Home</a>
+    </>
+  )
+}
+
 function App() {
-  retunr (
-    <h1>Midu Router</h1>
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+      
+    </main>
   )
 }
 
