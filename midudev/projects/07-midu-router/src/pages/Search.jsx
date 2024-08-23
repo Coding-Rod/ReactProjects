a96c8848fb3d37e83f0fdd535567db84160c9ee2
Coding-Rod <sergiorodrigofernandeztesta@gmail.com>
2024-08-23 01:05:49 -0400

    ✨ [ADD] QueryParams for Search

--- midudev/projects/07-midu-router/src/pages/Search.jsx
+++ midudev/projects/07-midu-router/src/pages/Search.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+
+function useQueryParams() {
+  const search = window.location.search;
+  const params = new URLSearchParams(search);
+  return Object.fromEntries(params.entries());
+}
+
 function SearchPage({ routeParams }) {
-  return <h1>Buscando {routeParams.query}</h1>
+  const { limit } = useQueryParams();
+
+  console.log({ limit });
+
+  useEffect(() => {
+    document.title = `Buscando ${routeParams.query}`;
+  }, []);
+
+  return <h1>Buscando {routeParams.query}</h1>;
 }
 
 export default SearchPage;
